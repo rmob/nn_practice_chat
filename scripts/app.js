cfa037b1ba369d39d56a740a8235fe7efa17b0df
rmob <riley.obryan@gmail.com>
2023-09-15 07:39:20 -0500

    updateName functionality added

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 // dom query
 const chatList = document.querySelector('.chat-list')
 const newChatForm = document.querySelector('.new-chat')
+const newNameForm = document.querySelector('.new-name')
+const updateMssg = document.querySelector('.update-mssg')
 
 // add a new chat
 newChatForm.addEventListener('submit', e => {
@@ -9,6 +11,17 @@ newChatForm.addEventListener('submit', e => {
     chatroom.addChat(message)
     .then(newChatForm.reset())
     .catch(err => console.log(err))
+})
+
+// update username
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const newName = newNameForm.name.value.trim()
+    chatroom.updateName(newName)
+    newNameForm.reset()
+    // show then hide update message
+    updateMssg.innerText = `Your name was updated to ${newName}`
+    setTimeout(() => updateMssg.innerText = '', 3000)
 })
 
 // class instances
